61a049042b40eb353dbff2a01bd2bb98d45b24b1
alexis <alexisalfaro505@gmail.com>
2025-08-06 17:40:34 -0600

    Change the app component for fetch the data from the search bar.
    The function handle city search is the one used to collect the data from
    the child, and then the state of the city will be updated so we can
    search for it in an API.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './components/searchBar';
+import { useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState("");
+
+  const handleCitySearch = (cityName) => {
+    console.log("Searching for city:", cityName);
+    setCity(cityName);
+    // pending for fetching weather data
+  };
+
   return (
-    <SearchBar />
+    <div>
+      <h1>Weather App</h1>
+      <SearchBar onSearch={handleCitySearch}/>
+      <p>Selected City: {city}</p>
+    </div>
+    
   );
 }
 
