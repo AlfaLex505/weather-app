34eaac8545434090e28f2b0cf9a7236cdda843b1
alexis <alexisalfaro505@gmail.com>
2025-08-07 16:46:38 -0600

    Correct the API key, add debug messages and passed the parameters to the weather display

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import SearchBar from './components/searchBar';
 import WeatherDisplay from './components/weatherDisplay'
 import { useState } from 'react';
 
-const API_KEY = '9ae1248b740e5b0137ff1c27beb963fb';
+// const API_KEY = '9ae1248b740e5b0137ff1c27beb963fb';
+const API_KEY = '9c5990ec93bd5380f4b02abfd5134c9d';
 
 function App() {
   const [temperature, setTemperature] = useState(null);
@@ -19,10 +20,17 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
+
         if (data.cod === "404") {
-          alert("City not found");
+          console.log("City not found");
           return;
         }
+        if (data.cod === 401) {
+          console.log("Invalid API ket");
+          return;
+        }
+
         setTemperature(data.main.temp);
         setCondition(data.weather[0].description);
         setIcon(data.weather[0].icon);
@@ -35,7 +43,11 @@ function App() {
     <div>
       <h1>Weather App</h1>
       <SearchBar onSearch={handleSearch}/>
-      <WeatherDisplay />
+      <WeatherDisplay 
+        temperature={temperature}
+        condition={condition}
+        icon={icon}
+        />
       <p>Selected City: {city}</p>
     </div>
     
